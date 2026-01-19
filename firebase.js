6d3085c95cd066428b5ff10f7391c0d071c678d0
shyamRamya-28 <shyamalaramya01@gmail.com>
2026-01-19 18:56:36 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,16 +1,15 @@
 // Your Firebase Configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCqfBL46Y5MZ_NFyedq8MTZOIbVvNR-Kog",
-  authDomain: "patient-health-monitorin-94ce1.firebaseapp.com",
-  databaseURL: "https://patient-health-monitorin-94ce1-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "patient-health-monitorin-94ce1",
-  storageBucket: "patient-health-monitorin-94ce1.firebasestorage.app",
-  messagingSenderId: "274102899124",
-  appId: "1:274102899124:web:a1bbcbf5bfce00a4e262e1"
+    apiKey: "AIzaSyCqfBL46Y5MZ_NFyedq8MTZOIBvNR-Kog",
+    authDomain: "patient-health-monitorin-94ce1.firebaseapp.com",
+    databaseURL: "https://patient-health-monitorin-94ce1-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "patient-health-monitorin-94ce1",
+    storageBucket: "patient-health-monitorin-94ce1.firebasestorage.app",
+    messagingSenderId: "274102899124",
+    appId: "1:274102899124:web:a1bbcb5fbfce00a4e262e1"
 };
 
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
-
 console.log("Firebase initialized successfully!");
